d044327a0a9086c6503b9a9080ea06d8655cc007
Adi Cang <mac@mb15n13nn.local>
2020-07-21 09:12:02 +0300

    Updates

---
+++ precache-manifest.ed22b86c957824cf2d7e03492546c5a8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "965b8f70a91bd22c826d2eb163f61538",
+    "url": "/footballData/index.html"
+  },
+  {
+    "revision": "738b45a8ef6bf1b724f3",
+    "url": "/footballData/static/css/main.e07878a3.chunk.css"
+  },
+  {
+    "revision": "b7cc9c3d16c0802cb3ce",
+    "url": "/footballData/static/js/2.129a1d3a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/footballData/static/js/2.129a1d3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "738b45a8ef6bf1b724f3",
+    "url": "/footballData/static/js/main.b5675854.chunk.js"
+  },
+  {
+    "revision": "06373d89788a9d6eff28",
+    "url": "/footballData/static/js/runtime-main.6b3c7410.js"
+  },
+  {
+    "revision": "41a2b4b25bbee358e37f0fe328ef717a",
+    "url": "/footballData/static/media/football.41a2b4b2.png"
+  },
+  {
+    "revision": "083a9a8d0677e19f8a9500d8a75bcd0b",
+    "url": "/footballData/static/media/loader.083a9a8d.gif"
+  },
+  {
+    "revision": "dcfa135e16099ca4a41df750fd7f41ae",
+    "url": "/footballData/static/media/shirt.dcfa135e.png"
+  }
+]);
